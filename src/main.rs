733e3bfd803ebfde26bd843b39a447962dbf4581
Peter Jaszkowiak <p.jaszkow@gmail.com>
2020-11-02 21:14:04 -0700

    repro with TcpStream in crate

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,26 @@
-#![feature(seek_convenience, read_initializer)]
+#![feature(seek_convenience, read_initializer, can_vector, unsafe_block_in_unsafe_fn)]
+
+#![allow(dead_code, unused_variables)]
 
 pub use std::cmp;
 pub use std::fmt;
-pub use std::net;
+pub use std::time;
+pub use std::convert;
+pub use std::ffi;
+pub use std::mem;
+pub use std::ptr;
+
 pub mod io {
     pub use std::io::*;
     pub const DEFAULT_BUF_SIZE: usize = 8 * 1024;
 }
 
 mod bufreader;
+mod tcp;
+mod net {
+    pub use crate::tcp::TcpStream;
+    pub use std::net::{Shutdown, SocketAddr, ToSocketAddrs};
+}
 
 fn main() {
     println!("Hello, world!");
